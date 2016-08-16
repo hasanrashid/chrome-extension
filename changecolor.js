28dcc59a8d4fa600898fb44db8d7f66382daace0
hasan rashid <hasan.j.rashid@gmail.com>
2016-08-16 19:52:30 -0400

    Added RGB to HSL conversion

--- changecolor.js
+++ changecolor.js
@@ -1,17 +1,78 @@
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     /*This onMessage listener listens for tabs.sendMessage. This is a content script specified
     in manifest.json */
-  
   if(message.msg=="getcolor"){
       /*Get the background color of the window in this script. This will return the 
       backgroundcolor of the tab. Getting the background color from popup.js will 
       return the color of popup.html*/
-       var bgc = window.getComputedStyle(document.body, null).backgroundColor;
-       var subs = bgc.substring(bgc.indexOf('(')+1,bgc.indexOf(')'));
-       var colors = subs.split(', ');
-       console.log(colors);
-       var fc = window.getComputedStyle(document.body, null).color;
-    sendResponse({red: colors[0], green: colors[1], blue: colors[2]});
+      var bgc = window.getComputedStyle(document.body, null).backgroundColor;
+      var subs = bgc.substring(bgc.indexOf('(')+1,bgc.indexOf(')'));
+      var colors = subs.split(', ');
+      var fc = window.getComputedStyle(document.body, null).color;
+      var rgbColors = {red: colors[0], green: colors[1], blue: colors[2]};
+
+      var red = parseInt(rgbColors.red);
+      var green = parseInt(rgbColors.green);
+      var blue = parseInt(rgbColors.blue);
+
+      hslColor = rgbToHsl(red,green,blue);
+      console.log(hslColor);
+      sendResponse(hslColor);
+
   }
   return true;
 });
+
+function rgbToHsl(r, g, b) {
+  
+   var min, max, i, l, s, maxcolor, h, rgb = [];
+
+   rgb[0] = r / 255;
+   rgb[1] = g / 255;
+   rgb[2] = b / 255;
+   min = rgb[0];
+   max = rgb[0];
+   maxcolor = 0;
+
+   for (i = 0; i < rgb.length - 1; i++) {
+     if (rgb[i + 1] <= min) {min = rgb[i + 1];}
+     if (rgb[i + 1] >= max) {max = rgb[i + 1];maxcolor = i + 1;}
+    }
+
+    if (maxcolor == 0) {
+      h = (rgb[1] - rgb[2]) / (max - min);
+    }
+
+    if (maxcolor == 1) {
+      h = 2 + (rgb[2] - rgb[0]) / (max - min);
+    }
+
+    if (maxcolor == 2) {
+      h = 4 + (rgb[0] - rgb[1]) / (max - min);
+    }
+
+    if (isNaN(h)) {
+      h = 0;
+    }
+
+    h = h * 60;
+
+    if (h < 0) {
+      h = h + 360; 
+    }
+
+    l = (min + max) / 2;
+
+    if (min == max) {
+      s = 0;
+    } else {
+      if (l < 0.5) {
+        s = (max - min) / (max + min);
+      } else {
+        s = (max - min) / (2 - max - min);
+      }
+    }
+    s = s;
+    return {h : h, s : s*100, l : l*100};
+}
--- popup.js
+++ popup.js
@@ -1,12 +1,21 @@
 
-
 document.addEventListener('DOMContentLoaded', function(){
+   var h = '';
+   var s = '';
+   var l = '' ;
+   var hslColor='';
     document.getElementById('btn').addEventListener('click',function(e){
       chrome.tabs.query({active: true, currentWindow: true},function(tabs){
         /*tabs.sendMessage sends message to content script that are part of the present tab */
      
       chrome.tabs.sendMessage(tabs[0].id,{msg:"getcolor"},function(response){
-        for(var i in response){
+        //console.log(response);
+        h = (response.h);
+        s = (response.s);
+        l = (response.l);
+
+       // console.log(h+' '+s+' ' +l);
+         for(var i in response){
             var controls = document.getElementsByClassName(i);
             controls[0].value = response[i];
             controls[1].value = response[i];
@@ -16,14 +25,13 @@ document.addEventListener('DOMContentLoaded', function(){
   });
  
   document.querySelectorAll('input[type="range"]').forEach(function(element) {
-    element.addEventListener('mouseup',function(e){
+    element.addEventListener('input',function(e){
       e.currentTarget.previousSibling.previousSibling.value = e.target.value;
     });
   }, this);
 
   document.querySelectorAll('input[type="radio"]').forEach(function(element) {
     element.addEventListener('change',function(e){
-      console.log(e.currentTarget);
       document.getElementById(e.currentTarget.value).disabled = true;
       ((e.currentTarget.parentNode).parentNode).disabled = false;
     });
